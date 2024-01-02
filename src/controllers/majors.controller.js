4c12d136b0384bf4e50e04a6e9eb519a0b73351f
Ayoub boudani <ayoubimajjati@gmail.com>
2024-01-02 16:33:28 +0100

    major controller fixed

--- src/controllers/majors.controller.js
+++ src/controllers/majors.controller.js
@@ -3,12 +3,34 @@ const asyncHandler = require('express-async-handler');
 const Major = require('../models/majors.model');
 const NotFoundError = require("../../Errors/NotFoundError");
 const { isValidObjectId } = require('mongoose');
+const mongoose = require('mongoose');
 const HttpError = require('../../Errors/HttpError');
 const { StatusCodes } = require('http-status-codes');
 
 const getAllMajors = asyncHandler(async (req, res, next) => {
     const { populate, min, max } = req.query;
-    const majors = await Major.find();
+
+    const majors = await Major.aggregate( [
+        {
+            $lookup: {
+                from: 'users',
+                localField: '_id',
+                foreignField: 'major',
+                as: 'users'
+            }
+        },
+        {
+            $lookup: {
+                from: 'exams',
+                localField: '_id',
+                foreignField: 'major',
+                as: 'exams'
+            }
+        },
+        { $unset: 'users.password' }
+    
+    ])
+    // const majors = await Major.find();
     res.status(StatusCodes.OK).send({
         "success": true,
         majors
@@ -20,16 +42,25 @@ const getMajor = asyncHandler(async (req, res, next) => {
 
     if (!isValidObjectId(id)) throw new HttpError("Invalid Id", 400)
 
-    const major = await Major.findById(id);
-
-    if (!major) throw new NotFoundError("Major not Found")
-
+    const major = await Major.aggregate( [
+        { $match: { _id : new mongoose.Types.ObjectId(id)  } },
+        
+        {
+            $lookup: {
+                from: 'exams',
+                localField: '_id',
+                foreignField: 'major',
+                as: 'exams'
+            }
+        }    
+    ])
     res.status(StatusCodes.OK).send({
-        "success": true,
-        major
+        success: true,
+        major: major[0]
     });
 
 });
+
 
 
 const deleteMajor = asyncHandler(async (req, res, next) => {
@@ -76,6 +107,39 @@ const createMajor = asyncHandler(async (req, res, next) => {
 });
 
 
+const getMajorUsers = asyncHandler(async (req,res,next) => {
+    const { id } = req.params;
 
-module.exports = { getAllMajors, getMajor, deleteMajor, modifyMajor, createMajor };
+    if (!isValidObjectId(id)) throw new HttpError("Invalid Id", 400);
+
+    const major = await Major.aggregate( [
+        { $match: { "_id" : new mongoose.Types.ObjectId(id) } },
+        {
+            $lookup: {
+                from: 'users',
+                localField: '_id',
+                foreignField: 'major',
+                as: 'users'
+            }
+        },
+        
+        { $unset: 'users.password' }
+    ])
+    if (!major || major.length === 0) {
+        return res.status(StatusCodes.NOT_FOUND).send({
+            success: false,
+            message: 'Major not found'
+        });
+    }
+
+    const users = major[0].users;
+    return res.status(StatusCodes.OK).send({
+        success: true,
+        users
+    });
+})
+
+
+
+module.exports = { getAllMajors, getMajor, deleteMajor, modifyMajor, createMajor, getMajorUsers };
 
--- src/routes/majors.route.js
+++ src/routes/majors.route.js
@@ -1,11 +1,12 @@
 
 const Router = require('express').Router;
-const { getAllMajors, getMajor, modifyMajor, deleteMajor, createMajor } = require('../controllers/majors.controller')
+const { getAllMajors, getMajor, modifyMajor, deleteMajor, createMajor, getMajorUsers } = require('../controllers/majors.controller')
 
 const route = Router();
 
 route.get('', getAllMajors);
 route.get('/:id', getMajor);
+route.get('/:id/users', getMajorUsers);
 route.post('/', createMajor);
 route.put('/:id', modifyMajor);
 route.delete('/:id', deleteMajor);
