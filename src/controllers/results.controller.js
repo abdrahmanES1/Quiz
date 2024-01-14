af1f0af4654c86a63c40a2486ed1d9b00f6175db
Ayoub boudani <ayoubimajjati@gmail.com>
2024-01-14 13:59:28 +0100

    #30 Creation of the results route (api)

--- src/controllers/results.controller.js
+++ src/controllers/results.controller.js
@@ -2,49 +2,74 @@
 const asyncHandler = require('express-async-handler');
 const Result = require('../models/results.model');
 
-const getAllResults = asyncHandler(async (req, res, next) => {
+const getAllResultsForExam = asyncHandler(async (req, res, next) => {
     const { populate, min, max } = req.query;
+    const { id } = req.params;
+
+    const results = await Result.find({"exam" : id}).populate("user");
+
+    if(!results) return res.send({ success : false, message : "No one passed the exam" });
 
     res.status(200).send({
         "success": true,
-
+        results
     });
 })
 
-const getResult = asyncHandler(async (req, res, next) => {
+const getResultsForStudent = asyncHandler(async (req, res, next) => {
     const { id } = req.params;
+
+    const results = await Result.find({ "user" : id }).populate("exam");
+    if (!results || results.length === 0) return res.send({ success : false, message : "U didn't pass any exam" })
     res.status(200).send({
         "success": true,
+        results
     });
 });
+
 
 const deleteResult = asyncHandler(async (req, res, next) => {
     const { id } = req.params;
 
+    const deletedResult = await Result.findByIdAndDelete(id);
     return res.status(200).send({
-        "success": true
+        "success": true,
+        message : "Result deleted succesfuly"
     });
 
 });
 
 const modifyResult = asyncHandler(async (req, res, next) => {
     const { id } = req.params;
+    const { grade } = req.body;
 
-    return res.status(200).send({
-        "success": true,
+    if(!await Result.findById(id)) return next(new NotFoundError('Result not found'));
 
+    await Result.updateOne( { "_id" : id } , { grade} );
+    res.status(200).send({
+        success : true,
+        message : "Result updated succesfuly"
     });
 });
 
 const createResult = asyncHandler(async (req, res, next) => {
     const { user, exam, grade, createdBy } = req.body;
 
+    const result = new Result({
+        user,
+        exam,
+        grade,
+        createdBy
+    })
+
+    await result.save();
     return res.status(200).send({
         "success": true,
+        result
     });
 });
 
 
 
-module.exports = { getAllResults, getResult, deleteResult, modifyResult, createResult };
+module.exports = { getAllResultsForExam, getResultsForStudent, deleteResult, modifyResult, createResult };
 
