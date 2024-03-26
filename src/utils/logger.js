50778479e9af70d15ea01fcea9484b1427bd5cbd
abdrahmanES1 <68553016+abdrahmanES1@users.noreply.github.com>
2024-03-26 22:29:22 +0000

    🚧 Fix : Logger folder path

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,15 +1,15 @@
 const winston = require("winston");
 const { transports } = winston;
 const { combine, timestamp, json } = winston.format
-
+const path = require("path")
 const logger = winston.createLogger({
     level: 'combined',
     format: combine(timestamp({
         format: 'YYYY-MM-DD hh:mm:ss.SSS A',
     }), json()),
     transports: [
-        new winston.transports.File({ filename: 'logs/error.log', level: 'error', format: winston.format.json(), }),
-        new winston.transports.File({ filename: 'logs/combined.log', level: 'info', format: winston.format.json(), })
+        new winston.transports.File({ filename:path.join(process.cwd(),'logs/error.log'), level: 'error', format: winston.format.json(), }),
+        new winston.transports.File({ filename: path.join(process.cwd(),'logs/info.log'), level: 'info', format: winston.format.json(), })
     ],
 });
 
